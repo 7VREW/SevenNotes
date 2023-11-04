a9c8d125edabde0cda8fad825fe718c0ab80dfb9
7VREW <sevenvrew@gmail.com>
2023-11-04 16:38:12 +0100

    Initial Commit

---
+++ SevenNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SevenNotes
+//
+//  Created by Émilio Williame on 04/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SevenNotes/SevenNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenNotesApp.swift
+//  SevenNotes
+//
+//  Created by Émilio Williame on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SevenNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
